68ac0d2304711dcef7bcabb284687fce28592562
Giorgi Chkhikvadze <giorgichxikvadze972@gmail.com>
2022-11-17 20:00:54 +0400

    changed port for deployment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors({ origin: true }));
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (_, res) => {
   res.send('Hello World!');
